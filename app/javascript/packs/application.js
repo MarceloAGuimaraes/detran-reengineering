39c9a9eb789b33ea645b40564a91ab0a8a21f920
Marcelo Guimarães <marceloguimaraesti@gmail.com>
2020-07-19 11:24:24 -0300

    init layout importation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
+require('jquery-tooltip/jquery.tooltip')
+require("bootstrap-material-design")
+require('packs/material-dashboard')
+require('@fortawesome/fontawesome-free')
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get '/' => 'home#index', as: :home
+
   namespace :drivers do
     get :check_punctuation
     get :track_request
