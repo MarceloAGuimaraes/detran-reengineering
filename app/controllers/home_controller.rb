f219f3d7497414433251bc90c3df4769a4386ab6
= <marceloguimaraesti@gmail.com>
2020-11-14 21:38:01 -0300

    home heroku bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,8 +18,8 @@ class HomeController < ApplicationController
   def check_news_updates
     return if New.updated?
 
-    #news_service = NewsService.new
-    #news_service.update_text_news
-    #news_service.update_img_news
+    news_service = NewsService.new
+    news_service.update_text_news
+    news_service.update_img_news
   end
 end
